1957ffdc6443d3e406bdb00e464f1621cac302ee
Ernesto Benito García <ernesto.benito722@gmail.com>
2023-04-23 18:54:09 +0200

    login, home

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
 
 }
