d9908ef64169b98d2a31b14996987f7d340b0a49
Ernesto Benito García <ernesto.benito722@gmail.com>
2023-04-23 18:22:47 +0200

    login, home

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,21 +3,23 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from "../shared/shared.module";
 
 
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  exports:[
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+    declarations: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    exports: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        SharedModule
+    ]
 })
 export class AuthModule { }
